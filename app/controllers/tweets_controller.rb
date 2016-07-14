ee724cc33579ecc66b086d29190d0f20e5a55fa1
Robert Luckfield <rluck0419@gmail.com>
2016-07-14 16:24:11 -0500

    crud stuff created for user/tweet, controllers filled out with methods

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,37 @@
 class TweetsController < ApplicationController
+  def index
+    render template: 'tweets/index.html.erb', locals: { tweets: Tweet.all }
+  end
+
+  def show
+    if Tweet.exists?(params[:id])
+      render template: 'tweets/show.html.erb', locals: { tweet: Tweet.find(params[:id]) }
+    else
+      render html: 'Tweet not found', status: 404
+    end
+  end
+
+  def new
+    render locals: {
+      tweet: Tweet.new
+    }
+  end
+
+  def create
+    tweet = Tweet.new
+    tweet.body = params[:tweet][:body]
+    tweet.user_id = params[:tweet][:user_id]
+    tweet.save
+    redirect_to "tweets/#{tweets.id}"
+  end
+
+  def destroy
+    if Tweet.exists?(params[:id])
+      Tweet.destroy(params[:id])
+      flash[:notice] = "Tweet destroyed."
+      redirect_to "tweets"
+    else
+      flash[:alert] = "There was an error. Please try again."
+    end
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def index
+    render template: 'users/index.html.erb', locals: { users: User.all }
+  end
+
+  def show
+    if User.exists?(params[:id])
+      render template: 'users/show.html.erb', locals: { user: User.find(params[:id]) }
+    else
+      render html: 'User not found', status: 404
+    end
+  end
+
+  def new
+    render locals: {
+      user: User.new
+    }
+  end
+
+  def create
+    user = User.new
+    user.name = params[:user][:name]
+    user.handle = params[:user][:handle]
+    user.email = params[:user][:email]
+    user.save
+    redirect_to "users/#{user.id}"
+  end
+
+  def destroy
+    if User.exists?(params[:id])
+      User.destroy(params[:id])
+      flash[:notice] = "User deleted."
+      redirect_to "users"
+    else
+      flash[:alert] = "There was an error. Please try again."
+    end
+  end
+end
