bf88ac01c49e940e6615c446961e596801deadee
Robert Luckfield <rluck0419@gmail.com>
2016-07-15 14:07:55 -0500

    basic homepage html and tweet index html moving along... now for remaining basic html pages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    render template: 'dashboard/index.html.erb', locals: { tweets: Tweet.all[0..24] }
+    render template: 'dashboard/index.html.erb', locals: { tweets: Tweet.all }
   end
 end
